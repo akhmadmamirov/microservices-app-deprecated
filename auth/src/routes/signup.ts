cd48f2a8beb2d0655a3f04352438a9c19bf27fce
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-03 16:55:37 -0800

    refactoring errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, {Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+
 
 const router = express.Router()
 
@@ -17,13 +20,13 @@ router.post('/api/users/signup',
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
-      throw new Error('Invalid email or password')
+      throw new RequestValidationError(errors.array())
     }
 
     const {email, password} = req.body
 
     console.log('Creating a user...')
-    throw new Error('Error connecting to DB')
+    throw new DatabaseConnectionError()
     res.send({})
 })
 
